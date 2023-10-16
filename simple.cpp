d2feba9a7e18278321927842b95f25dc062bd7b5
Sampath Vinay <sampath1997vinay@gmail.com>
2023-10-16 18:05:58 -0400

    Updated simple.cpp on master for conflicts

--- simple.cpp
+++ simple.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(){
 
-cout<<"Hello there! Master branch welcomes you"<<endl;
+cout<<"Hello there! Master branch welcomes you. Hope you have a wonderful time here"<<endl;
 
 return 0;
 
